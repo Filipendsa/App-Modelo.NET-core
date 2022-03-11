2a8adfa8a2dc92c0f2054ea20018463f4946b664
IATec - Filipe Nogueira <filipe.nogueira@iatec.com>
2022-03-11 09:35:47 -0300

    Trabalhando com o Crud

--- src/DevIo.UI.Site/Controllers/TesteCrudController.cs
+++ src/DevIo.UI.Site/Controllers/TesteCrudController.cs
@@ -1,4 +1,5 @@
-﻿using DevIo.UI.Site.Models;
+﻿using DevIo.UI.Site.Data;
+using DevIo.UI.Site.Models;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -9,6 +10,11 @@ namespace DevIo.UI.Site.Controllers
 {
     public class TesteCrudController : Controller
     {
+        public TesteCrudController(MeuDbContext contexto)
+        {
+            _contexto = contexto;
+        }
+        private readonly MeuDbContext _contexto; 
         public IActionResult Index()
         {
             var aluno = new Aluno {
@@ -16,6 +22,20 @@ namespace DevIo.UI.Site.Controllers
                 DataNascimento = DateTime.Now,
                 Email = "teste@gmail.com"
             };
+
+            _contexto.Alunos.Add(aluno);
+            _contexto.SaveChanges();
+
+            var aluno2 = _contexto.Alunos.Find(aluno.Id);
+            var aluno3 = _contexto.Alunos.FirstOrDefault(a=>a.Email == "teste@gmail.com"); //o primeiro dado
+            var aluno4 = _contexto.Alunos.Where(a => a.Nome == "Filipe");//todos os resultados
+
+            aluno.Nome = "joão";
+            _contexto.Alunos.Update(aluno);
+            _contexto.SaveChanges();
+
+            _contexto.Alunos.Remove(aluno);
+            _contexto.SaveChanges();
 
             return View();
         }
